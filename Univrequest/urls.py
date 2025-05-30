29734994a8b364cd70baff42dfa2da84fd851069
Bel-Ami8 <josepharmelbessala@email.com>
2025-05-30 20:29:25 +0100

    Mise à jour des templates et des styles

--- Univrequest/urls.py
+++ Univrequest/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 from Univrequest.views import home,register,receptionniste,etudiant
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("register", views.register, name="register"),
     path("receptionniste", views.receptionniste, name="receptionniste"),
-    path("etudiant", views.etudiant, name="etudiant")
+    path("etudiant", views.etudiant, name="etudiant"),
+    path("logout/", auth_views.LogoutView.as_view(next_page="home"), name="logout"),
 ]
--- Univrequest/views.py
+++ Univrequest/views.py
@@ -1,25 +1,37 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate,login
+from django.contrib import messages
+from django.contrib.auth import authenticate,login,logout
 
 from Univrequest.form import CustomUserCreationForm
 
 
 
 def home(request):
-    error= ''# Create your views here.
-    if request.method == "POST":
-        username = request.POST['username']
-        password = request.POST['password']
-        user = authenticate(request, username = username, password = password)
-        if user is not None :
-            login(request, user)
-            if user.is_etudiant:
-                return redirect('etudiant')
-            elif user.is_receptionniste:
-                return redirect('receptionniste')
-        else:
-            error = 'mot de passe ou utilisateur incorrect'
-    return render(request, 'login.html', {'error':error})
+    if request.user.is_authenticated:
+        # Redirige directement vers la page utilisateur appropriée
+        if request.user.is_etudiant:
+            return redirect('etudiant')
+        elif request.user.is_receptionniste:
+            return redirect('receptionniste')
+        # ou render une autre page d'accueil
+    else:
+        # Sinon, affiche la page de login
+        error = ''
+        if request.method == "POST":
+            username = request.POST['username']
+            password = request.POST['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                if user.is_etudiant:
+                    return redirect('etudiant')
+                elif user.is_receptionniste:
+                    return redirect('receptionniste')
+            else:
+                error = 'mot de passe ou utilisateur incorrect'
+        return render(request, 'login.html', {'error': error})
+
+
 
 
 def register(request):
@@ -38,3 +50,9 @@ def etudiant(request):
 
 def receptionniste(request):
     return render(request,'receptionniste.html')
+
+
+def logout_view(request):
+    logout(request)
+    messages.success(request, "Vous avez été déconnecté.")
+    return redirect('home')
